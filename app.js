fbecd42ceb4dd4e79f9dbd15c59b6437053f1e54
Torea Patissier <p.torea@icloud.com>
2022-11-03 13:50:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express'); // Framework
+const mogran = require('morgan'); // To access req,res,next
+const bodyParser = require('body-parser'); // To parse de request
+const sequelize = require('sequelize'); //ORM
